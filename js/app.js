cf5068ca6e2b2d2e1c8f9aeaf2b626483cb41bee
Ryan Walker <ryanwalker@Emilys-MacBook-Air.local>
2018-07-30 12:24:28 -0400

    addition works

--- js/app.js
+++ js/app.js
@@ -1,50 +1,112 @@
 class Calculator {
   constructor() {
-    this.num = 0;
+    this.total = 0;
     this.operation = '';
   }
   update(x) {
-    this.num = x;
+    this.total = x;
   }
   add(x) {
-    return (this.num = this.num + x);
+    return (this.total = this.total + x);
   }
   subtract(x) {
-    return (this.num = this.num - x);
+    return (this.total = this.total - x);
   }
   multiply(x) {
-    return (this.num = this.num * x);
+    return (this.total = this.total * x);
   }
   divide(x) {
-    return (this.num = this.num / x);
+    return (this.total = this.total / x);
   }
   clear() {
-    this.num = 0;
+    this.total = 0;
   }
 }
 
+const ui = new UI();
 const calculator = new Calculator();
 
-const inputValue = document.querySelectorAll('.input-value');
+const inputNumber = document.querySelectorAll('.number');
 const output = document.querySelector('.output');
+const container = document.querySelector('.container');
+
+container.addEventListener('click', e => {
+  console.log(e.target);
+});
+
+inputNumber.forEach(value => {
+  value.addEventListener('click', () => {
+    if (ui.output.textContent === '0') {
+      // remove zero from interface
+      ui.clearOutput();
+      // change text content to C
+      ui.clearBtn.textContent = 'C';
+      // add totalber to interface
+      ui.output.textContent += value.textContent;
+    } else if (calculator.operation === '') {
+      // add totalber to interface
+      ui.output.textContent += value.textContent;
+    } else {
+      // remove number from interface
+      ui.clearOutput();
+      // add totalber to interface
+      ui.output.textContent += value.textContent;
+    }
+  });
+});
+
+ui.addBtn.addEventListener('click', () => {
+  // show engaged
+  ui.addBtn.classList.add('highlight');
+  // update operation
+  calculator.operation = '+';
+  // add output value to calculator
+  calculator.update(parseInt(ui.output.textContent));
+});
+
+ui.equalsBtn.addEventListener('click', () => {
+  if (calculator.operation === '+') {
+    calculator.add(parseInt(ui.output.textContent));
+    ui.output.textContent = calculator.total;
+  } else if (calculator.operation === '-') {
+    calculator.subtract(parseInt(ui.output.textContent));
+  } else if (calculator.operation === '×') {
+    calculator.multiply(parseInt(ui.output.textContent));
+  } else if (calculator.operation === '÷') {
+    calculator.divide(parseInt(ui.output.textContent));
+  }
+});
+
+/*
 
 inputValue.forEach(value => {
   value.addEventListener('click', e => {
     let n = '';
     if (value.textContent === 'C' || value.textContent === 'AC') {
       calculator.clear();
-      console.log(calculator.num);
+      console.log(calculator.total);
     } else if (value.textContent === '=') {
-      console.log(calculator.num);
+      console.log(calculator.total);
     } else if (value.textContent === '+') {
+      calculator.update(parseInt(ui.output.textContent))
       calculator.operation('+');
     } else {
-      n += value.textContent;
-      output.textContent += n;
-      // if (calculator.num === 0) {
+      // start of new calculation
+      if (ui.output.textContent === '0') {
+        ui.clearOutput();
+        n += value.textContent;
+        output.textContent += n;
+      } else {
+        n += value.textContent;
+        output.textContent += n;
+      }
+
+      // if (calculator.total === 0) {
       //   calculator.update(n);
       // }
     }
-    // console.log(number.textContent);
+    // console.log(totalber.textContent);
   });
 });
+
+*/
--- js/ui.js
+++ js/ui.js
@@ -2,11 +2,18 @@ class UI {
   constructor() {
     this.output = document.querySelector('.output');
     this.inputValues = document.querySelectorAll('.input-value');
+    this.clearBtn = document.querySelector('#clearBtn');
+    this.addBtn = document.querySelector('#addBtn');
+    this.equalsBtn = document.querySelector('#equalsBtn');
   }
   updateOutput(inputValue) {
     this.output.innerHTML = inputValue;
   }
   resetOutput() {
-    this.output.textContent = 0;
+    this.output.textContent = '0';
+  }
+
+  clearOutput() {
+    this.output.textContent = '';
   }
 }
